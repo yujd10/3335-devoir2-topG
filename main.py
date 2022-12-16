f1ee9512c1d6b0cc8ca94ffa782f95023bd59cc4
yujd10 <73360330+yujd10@users.noreply.github.com>
2022-12-16 15:19:34 -0500

    2 helper functions (not sure if useful or not)

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+
+def find_nom_groups(str):
+    return re.findall(r'\[.*?\]', str)
+
+def find_interests(str):
+    return re.findall(r'(?i)\*?interests?_?[0-9]?', str)
+
+if __name__ == "__main__":
+    count = 0
+    phrases = []
+    with open("corpus.txt") as file:
+        for i in file :
+            if i!="$$\n":
+                phrases.append(i)
+    phrase_1 =phrases[0]
+    phrase_2 = "====================================== [ odds/NNS ] and/CC [ ends/NNS ] ====================================== despite/IN [ growing/VBG Interest_1/NN ] in/IN [ the/DT environment/NN ] ,/, [ u.s./NP consumers/NNS ] have/VBP n't/RB shown/VBN [ much/JJ *IntErest/NN ] in/IN [ refillable/JJ packages/NNS ] for/IN [ household/NN products/NNS ] ./. "
+    print(phrase_1)
+    GNs = find_interests(phrase_2)
+    print(GNs)
